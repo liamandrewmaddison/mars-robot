ad9b50a330b7ce20eff9dc545c288ae3cf100a61
Liam Maddison <maddison.liam@gmail.com>
2024-03-05 00:23:17 +0000

    Adding 100% coverage

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ We don't need to `npm install` as I uploaded them given the size of the app, so 
 
 - In your terminal run, `node robot.js`
 - Alternatively you may run, `npm run test` to test our outputs
+
+### Coverage Report
+| File      | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s |
+|-----------|---------|----------|---------|---------|-------------------|
+| All files |     100 |      100 |     100 |     100 |                   |
+| robot.ts  |     100 |      100 |     100 |     100 |                   |
+
+Test Suites: 1 passed, 1 total
+Tests:       6 passed, 6 total
+Snapshots:   0 total
+Time:        0.178 s, estimated 1 s
--- robot.spec.ts
+++ robot.spec.ts
@@ -1,5 +1,4 @@
-import { Grid, Position, Robot } from "./robot";
-import { Direction } from "./robot";
+import { Grid, Position, Robot, Direction } from "./robot.ts";
 
 describe('Mars Robot', () => {
   const grid: Grid = { maxX: 5, maxY: 3, scents: new Set<string>() };
@@ -35,5 +34,24 @@ describe('Mars Robot', () => {
     const robot = new Robot(grid, position);
 
     expect(robot.processInstructions('LLFFFLFLFL')).toBe('2 3 S');
+  });
+
+  it(`should throw an error for instructions with more than 100 characters`, () => {
+    const position: Position = { x: 0, y: 3, direction: 'W' as Direction };
+    const robot = new Robot(grid, position);
+
+    expect(() => robot.processInstructions(`L`.repeat(101))).toThrow('Instructions cannot be more than 100 characters');
+  });
+
+  it(`should throw an error for position coordinates x more than 50`, () => {
+    const position: Position = { x: 51, y: 0, direction: 'W' as Direction };
+
+    expect(() => new Robot(grid, position)).toThrow('Position coordinates cannot be more than 50');
+  });
+
+  it(`should throw an error for position coordinates y more than 50`, () => {
+    const position: Position = { x: 0, y: 51, direction: 'W' as Direction };
+
+    expect(() => new Robot(grid, position)).toThrow('Position coordinates cannot be more than 50');
   });
 });
